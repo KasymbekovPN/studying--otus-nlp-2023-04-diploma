032afc703771ddff06baf3da2abbade4d558ab83
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-08-31 22:30:38 +0300

    Result is done

--- m-resume-rater/src/result/result.py
+++ m-resume-rater/src/result/result.py
@@ -1,20 +1,66 @@
 
 class Status:
-    def __init__(self, template: str, **kwargs):
+    def __init__(self, template: str, **kwargs) -> None:
         self._template = template
         self._args = kwargs
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'Status {{ template: {self._template}, args: {self._args} }}'
 
+    def __eq__(self, other):
+        if type(self) != type(other):
+            return False
+        return self.template == other.template and self.args == other.args
+
     @property
-    def template(self):
+    def template(self) -> str:
         return self._template
 
     @property
-    def args(self):
+    def args(self) -> dict:
         return self._args
 
 
 class Result:
-    pass
+    def __init__(self, success: bool, value, statuses: list[Status]) -> None:
+        self._success = success if success is not None else False
+        self._value = value
+        self._statuses = statuses if statuses is not None else []
+
+    def __repr__(self) -> str:
+        return f'Result {{ success: {self.success}, value: {self.value}, statuses: {self.statuses} }}'
+
+    def __eq__(self, other):
+        if type(self) != type(other):
+            return False
+        return self.success == other.success and \
+            self.value == other.value and \
+            self.statuses == other.statuses
+
+    @property
+    def success(self) -> bool:
+        return self._success
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def statuses(self) -> list[Status]:
+        return self._statuses
+
+    @staticmethod
+    def ok(value):
+        return Result(True, value, [])
+
+    @staticmethod
+    def fail(status: Status):
+        return Result(False, None, [status])
+
+    @staticmethod
+    def simple_fail(template: str):
+        return Result(False, None, [Status(template)])
+
+    @staticmethod
+    def full(success: bool, value, statuses: list[Status]):
+        return Result(success, value, statuses)
--- m-resume-rater/test/result/result_test.py
+++ m-resume-rater/test/result/result_test.py
@@ -16,6 +16,68 @@ class TestCase(unittest.TestCase):
         self.assertEqual(template, status.template)
         self.assertEqual(expected_args, status.args)
 
+    @parameterized.expand([
+        (True,),
+        (False,)
+    ])
+    def test_success_getter(self, expected_success: bool):
+        r = Result(expected_success, None, [])
+        self.assertEqual(r.success, expected_success)
+
+    @parameterized.expand([
+        (1,),
+        ("1",),
+        ({"1": 1},),
+        None
+    ])
+    def test_value_getter(self, expected_value):
+        r = Result(True, expected_value, [])
+        self.assertEqual(r.value, expected_value)
+
+    @parameterized.expand([
+        ([Status("template.0")], [Status("template.0")]),
+        (
+                [Status("template.0"), Status("template.0", x=123)],
+                [Status("template.0"), Status("template.0", x=123)]
+        ),
+        (None, [])
+    ])
+    def test_statuses_getter(self, statuses, expected_statuses):
+        r = Result(False, None, statuses)
+        self.assertEqual(r.statuses, expected_statuses)
+
+    @parameterized.expand([
+        (123,),
+        ("123",)
+    ])
+    def test_ok_result_creation(self, value):
+        r = Result.ok(value)
+        self.assertEqual(r, Result(True, value, []))
+
+    @parameterized.expand([
+        (Status("template.0"),),
+        (Status("template.1", x=123),)
+    ])
+    def test_fail_result_creation(self, status: Status):
+        r = Result.fail(status)
+        self.assertEqual(r, Result(False, None, [status]))
+
+    @parameterized.expand([
+        ("template.0",),
+        ("template.1",)
+    ])
+    def test_simple_fail_result_creation(self, template: str):
+        r = Result.simple_fail(template)
+        self.assertEqual(r, Result(False, None, [Status(template)]))
+
+    @parameterized.expand([
+        (False, None, [Status("template.0")]),
+        (True, 123, [])
+    ])
+    def test_full_result_creation(self, expected_success: bool, expected_value, expected_statuses: list):
+        r = Result.full(expected_success, expected_value, expected_statuses)
+        self.assertEqual(r, Result(expected_success, expected_value, expected_statuses))
+
 
 if __name__ == '__main__':
     unittest.main()
