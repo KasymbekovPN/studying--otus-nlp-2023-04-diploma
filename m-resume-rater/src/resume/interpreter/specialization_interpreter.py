ca2f2558109c1ca1d168125f860fb0075b8ad676
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-09-02 18:47:56 +0300

    SpecializationInterpreter is done

--- m-resume-rater/src/resume/interpreter/specialization_interpreter.py
+++ m-resume-rater/src/resume/interpreter/specialization_interpreter.py
@@ -1,4 +1,12 @@
+from src.result import Result
+from src.resume.interpreter import BaseInterpreter
 
-class SpecializationInterpreter:
-    # todo impl
-    pass
+
+class SpecializationInterpreter(BaseInterpreter):
+    def raw(self) -> Result:
+        specialization = self.resume.specialization
+        return Result.simple_fail('resume.specialization.absence', id=self.resume.resume_id) \
+            if specialization is None \
+            else Result.ok(specialization)
+
+    #  todo off ngrams/nwords ???
