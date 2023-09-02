f1853e79b7691a552741db9390ad9f5412b975c5
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-09-02 18:34:46 +0300

    EducationInterpreter is done

--- m-resume-rater/src/resume/interpreter/education_interpreter.py
+++ m-resume-rater/src/resume/interpreter/education_interpreter.py
@@ -1,4 +1,10 @@
+from src.result import Result
+from src.resume.interpreter import BaseInterpreter
 
-class EducationInterpreter:
-    # todo impl
-    pass
+
+class EducationInterpreter(BaseInterpreter):
+    def raw(self) -> Result:
+        education = self.resume.education
+        return Result.simple_fail('resume.education.absence', id=self.resume.resume_id) \
+            if education is None \
+            else Result.ok(education)
