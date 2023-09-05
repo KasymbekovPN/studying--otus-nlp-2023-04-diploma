0d65a9981ddd2ee0139de95d448be9d896e498c9
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-09-05 22:21:23 +0300

    simple flask start is added

--- m-resume-rater/src/app.py
+++ m-resume-rater/src/app.py
@@ -1,2 +1,25 @@
+from flask import Flask, request
 
-# todo add Flask app
+
+HOST = 'localhost'
+PORT = 5000
+FLASK_ABORT_CODE = 403
+ROUTE_RULE = '/'
+ROUTE_METHODS = ['POST', 'GET']
+
+
+def run():
+    app = Flask(__name__)
+
+    @app.route(ROUTE_RULE, methods=ROUTE_METHODS)
+    def index():
+        x = request
+        print(x)
+        return ''
+
+    app.run(HOST, PORT)
+
+
+if __name__ == '__main__':
+    run()
+    
