5540508efb9b9f3af3b01ca33e67637e234a1b47
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-09-12 21:57:52 +0300

    - clr

--- m-resume-rater/src/utils/__init__.py
+++ m-resume-rater/src/utils/__init__.py
@@ -1,3 +1,2 @@
-from src.utils.ngrams_computer import NGramsComputer
 from src.utils.nwords_computer import NWordsComputer
 from src.utils.torch_device import get_torch_device
--- m-resume-rater/src/utils/ngrams_computer.py
+++
@@ -1,7 +0,0 @@
-from src.result import Result
-
-
-# todo del
-class NGramsComputer:
-    def compute(self, inputs: tuple) -> Result:
-        pass
